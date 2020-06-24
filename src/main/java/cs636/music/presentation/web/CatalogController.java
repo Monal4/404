e593ec4c23b2cf6be35f1e8aa6d60caade8ec3c3
Monal4 <51195063+Monal4@users.noreply.github.com>
2020-06-24 16:10:01 -0400

    Fixed Cart

--- src/main/java/cs636/music/presentation/web/CatalogController.java
+++ src/main/java/cs636/music/presentation/web/CatalogController.java
@@ -64,8 +64,6 @@ public class CatalogController{
 		Product product = null;
 		Cart cart = null;
 
-		System.out.println("The quantity is:- " + productQuantity);
-
 		try {
 			product = catalogService.getProductByCode(productCode);
 		} catch (Exception e) {
@@ -84,11 +82,6 @@ public class CatalogController{
 			throw new ServletException(e);
 		}
 
-	
-		//catalogService.addItemtoCart(product.getId(), cart, productQuantity);
-		// System.out.println("Item added to cart");
-		
-
 		return "catalog";
 	}
 
@@ -99,34 +92,16 @@ public class CatalogController{
 			return "userWelcome";
 		}
 		Set<CartItemData> setofcartdata = new HashSet<CartItemData>();
-		// BigDecimal bg = new BigDecimal("50.00");
-		// CartItemData cv = new CartItemData(1,5,"Code","Desc",bg);
-		// setofcartdata.add(cv);
-		System.out.println("Test1");
 		
 		Cart cart = null;
 		try {
 			cart = (Cart) request.getSession().getAttribute("cart");
 			cart.getItems().forEach(i->System.out.println(i.getProductId()));
-			// if (checkCart(request) == false) {
-			// 	cart = catalogService.createCart();
-			// }
 			setofcartdata = catalogService.getCartInfo(cart);
-			// setofcartdata.forEach((item)->{
-			// 	System.out.println("the code"+item.getCode());
-			// 	System.out.println("description test"+item.getDescription());
-			// 	System.out.println(item.getProductId());
-			// });
-			// if(checkCart(request) == false) {
-			// 	request.getSession().setAttribute("cart", cart);
-			// }
-			//model.addAttribute("allProducts", setofcartdata);
 		}catch(Exception e) {
 			System.out.println("Exception in setofcartdata" + e);
 		}	
 		model.addAttribute("products", setofcartdata);
-		request.setAttribute("products", setofcartdata);
-		//setofcartdata.forEach(i->System.out.println("1123"+i.getProductId()));
 		return "cart";
 	}
 	
--- src/main/webapp/cart.jsp
+++ src/main/webapp/cart.jsp
@@ -1,4 +1,8 @@
+<%@page language="java" contentType="text/html; charset=UTF-8"
+pageEncoding="UTF-8"%>
 <jsp:include page="/includes/header.jsp" />
+
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 
     <table class="table">
       	<thead>
